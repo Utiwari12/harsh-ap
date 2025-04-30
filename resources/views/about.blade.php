a961ace3897722d66066d09a75fbd106dbf136f3
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-04-30 19:10:46 +0530

    Include condition

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,6 @@
+@include('common.header')
 <h1>About</h1>
 
+@includeIf('common.inner', ['page'=>'This is about Page of Common Inner Page'])
+{{-- @include('common.inner', ['page'=>'This is about Page of Common Inner Page']) --}}
 {{-- <h2>{{ $name }} </h2> --}}
