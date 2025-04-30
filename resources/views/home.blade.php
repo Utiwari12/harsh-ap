42a951fb9d180e77b4aae28d340419c98f50dd51
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-04-30 17:55:52 +0530

    home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,3 +3,31 @@
 <a href="/about">About</a>
 
 <a href="/">Welcome Page</a>
+
+<h2>{{ $name }}</h2>
+{{-- <h2>{{ rand(1, 100) }}</h2> --}}
+
+{{-- <h3>{{ $users[2] }}</h3> --}}
+
+@if ($name == 'Upendra Tiwari')
+    <h3>{{ $name }}</h3>
+@elseif ($name == 'Nilam Tiwari')
+    <h2>{{ $name }}</h2>
+@else
+    <h2>Other Users</h2>
+@endif
+
+<div>
+   @foreach ($users as $user)
+       <h3>{{ $user }}</h3>
+   
+   @endforeach
+</div>
+
+<div>
+    @for ($i = 0; $i < 10; $i++)
+        <h2>{{ $i }}</h2>
+    
+    @endfor
+</div>
+
